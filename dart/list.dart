cec8abf75daaf66ee6d3ebd9d00a83b731c18941
marijoyapa <marijoy.apa@tooltwist.com>
2024-01-10 11:49:56 +0800

    dart basic variables, objects

---
+++ dart/list.dart
@@ -0,0 +1,9 @@
+void main() {
+  List<String> stringList = ['apple', 'banana', 'orange'];
+
+  // Accessing specific elements
+  String specificString = stringList[2]; // Accessing the element 'orange'
+
+  // Printing the specific elements
+  print('Specific element in stringList: $specificString');
+}
