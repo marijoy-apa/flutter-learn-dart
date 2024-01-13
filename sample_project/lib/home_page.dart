52f1aeab57a505903eeb871c4eec9fecd6c8fa45
marijoyapa <marijoy.apa@tooltwist.com>
2024-01-13 22:18:32 +0800

    create and use basic provider

--- sample_project/lib/home_page.dart
+++ sample_project/lib/home_page.dart
@@ -1,21 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:sample_project/data/data.dart';
 import 'package:sample_project/product_box.dart';
+import 'package:sample_project/provider/product_list.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends ConsumerWidget {
   const MyHomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final productProvider = ref.watch(productListProvider);
+
     return Scaffold(
         appBar: AppBar(
           title: const Text('Product List'),
         ),
         drawer: const Drawer(),
         body: ListView.builder(
-          itemCount: productList.length,
+          itemCount: productProvider.length,
           itemBuilder: (ctx, index) => ProductBox(
-            product: productList[index],
+            product: productProvider[index],
           ),
         ));
   }
--- sample_project/lib/main.dart
+++ sample_project/lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sample_project/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -19,6 +20,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-
-
