0488786a60cbed9d03832e5bf32121bc0ab72231
marijoyapa <marijoy.apa@tooltwist.com>
2024-01-14 18:16:47 +0800

    Update home_page.dart

--- sample_project/lib/home_page.dart
+++ sample_project/lib/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:sample_project/data/data.dart';
 import 'package:sample_project/product_box.dart';
 import 'package:sample_project/provider/product_list.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -12,15 +11,16 @@ class MyHomePage extends ConsumerWidget {
     final productProvider = ref.watch(productListProvider);
 
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Product List'),
+      appBar: AppBar(
+        title: const Text('Product List'),
+      ),
+      drawer: const Drawer(),
+      body: ListView.builder(
+        itemCount: productProvider.length,
+        itemBuilder: (ctx, index) => ProductBox(
+          product: productProvider[index],
         ),
-        drawer: const Drawer(),
-        body: ListView.builder(
-          itemCount: productProvider.length,
-          itemBuilder: (ctx, index) => ProductBox(
-            product: productProvider[index],
-          ),
-        ));
+      ),
+    );
   }
 }
