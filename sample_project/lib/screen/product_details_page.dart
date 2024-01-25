de78276becb46ea2fd7854cbde7c775e383c7f96
marijoyapa <marijoy.apa@tooltwist.com>
2024-01-25 11:36:18 +0800

    covert productDetails widget to ConsumerWidget

--- sample_project/lib/screen/product_details_page.dart
+++ sample_project/lib/screen/product_details_page.dart
@@ -1,15 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sample_project/model/product.dart';
+import 'package:sample_project/provider/product_list_provider.dart';
 
-class ProductDetails extends StatelessWidget {
+class ProductDetails extends ConsumerWidget {
   const ProductDetails({super.key, required this.product});
 
   final Product product;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    Product productItem = product;
+    productItem = ref
+        .watch(productListProvider)
+        .where((list) => list.id == product.id)
+        .toList()[0];
+
     return Scaffold(
-      appBar: AppBar(title: Text(product.name)),
+      appBar: AppBar(
+        title: Text(product.name),
+        actions: [
+          IconButton(
+            onPressed: () {
+              ref
+                  .read(productListProvider.notifier)
+                  .onAddFavorites(productItem);
+            },
+            icon: Icon(productItem.isFavorites
+                ? Icons.favorite
+                : Icons.favorite_outline),
+          ),
+        ],
+      ),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
